d45f23df4e7745bfdc23cc259191283ebcb42221
Eamon <eamon0989@gmail.com>
2022-01-12 09:24:48 +0100

    removed extra CSS

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <div className="nine columns main-col">
               <h2>About Me</h2>
 
-              {bio.map(par => <p>{par}</p>)}
+              {bio.map((par, index) => <p key={index}>{par}</p>)}
               
               <div className="row">
                 <div className="columns download">
